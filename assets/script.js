1717ae40f83f07d7c280ad2b6bd3dc4f5b764f01
casanaa <antoniocasana@yahoo.com>
2023-07-19 19:43:07 -0500

    Added cat API

--- assets/script.js
+++ assets/script.js
@@ -53,4 +53,21 @@ function createPlaceCard(place) {
 
 searchBarEL.on('submit', handleFormSubmit)
 
+document.addEventListener("DOMContentLoaded", () => {
+  fetchCatImage()
+  btnClick();
+})
+
+function fetchCatImage(){
+  let image = document.getElementById("cat-image")
+  fetch('https://api.thecatapi.com/v1/images/search')
+  .then(resp => resp.json())
+  .then(json => image.src = json[0].url)
+}
+
+function btnClick(){
+  let button = document.getElementById("new-cat-btn")
+  button.addEventListener("click", fetchCatImage)
+}
+
 
